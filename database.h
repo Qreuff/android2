039a0bd29aeaf020375d1e928897f09d824cbb0c
Qreuff <anatoliyhonor26@gmail.com>
2026-05-28 08:13:07 +0700

    update project

--- database.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <atomic>
-
-void init_database();
-void save_packet(const std::string& raw_json);
-void sync_all_data();
-void migrate_json_to_sql();
-bool check_db_alive();
